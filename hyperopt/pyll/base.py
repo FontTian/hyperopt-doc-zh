23f4145265a0476bf43c5f29c9acf27a46d03328
James Bergstra <james.bergstra@gmail.com>
2013-05-27 17:43:34 -0400

    fix: sort keys in kwswitch

--- hyperopt/pyll/base.py
+++ hyperopt/pyll/base.py
@@ -1109,7 +1109,8 @@ def switch(pos, *args):
 def _kwswitch(kw, **kwargs):
     """conditional evaluation according to string value"""
     # Get the index of the string in kwargs to use switch
-    keys, values = zip(*kwargs.items())
+    keys = list(sorted(kwargs))
+    values = [kwargs[k] for k in keys]
     match_idx = scope.call_method_pure(keys, 'index', kw)
     return scope.switch(match_idx, *values)
 
